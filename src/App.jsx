f9a6c0297ae876997e5782dff6ff53d49e2908c8
jatin-rajput563 <jkt3298@gmail.com>
2025-06-06 09:30:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import NavSlider from "./components/NavSlider";
+
+function App() {
+  return (
+    <>
+      <NavSlider />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NavSlider.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavSlider = () => {
+  return (
+    <>
+      <div className=""></div>
+    </>
+  );
+};
+
+export default NavSlider;
